587a992520cdaff5819364a600155c1e7a5de272
Hacker-Feacbook <110688575+Hacker-Feacbook@users.noreply.github.com>
2022-08-28 01:46:17 +0100

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,26 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;32m')
+    os.system ('clear')
+    os.system ('figlet hak insta ')
+    print ("")
+    print (" \033[1;36mﻟﻤﮑﺗﻟ بﺎﺴﺗو يﻓ توﺑ مﻗر ﻰﻠﻋ مھرد 100 ﻟﺴرأ")
+    print ("")
+    print (" \033[1;32m  : مﻗر")
+    print ("")
+    time.sleep(0.2)
+    print (" \033[1;30mﺔﯾﺤﺿ بﺎﺴﺤ مﺴإ  ==> @ 🚫")
+    time.sleep(0.2)
+    os.system ('figlet')
+main (main)
+home_main ()
